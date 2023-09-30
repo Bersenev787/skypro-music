b08931bb6d0dc4c7e8e869bed4915f574f455af4
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-09-30 16:38:52 +0300

    hw-react-components: components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Bar from "./components/bar/Bar";
 import Filter from "./components/filter/Filter";
 import Footer from "./components/footer/Footer";
 import MainNav from "./components/main-nav/MainNav";
+import PlaylistTitle from "./components/playlist-title/PlaylistTitle";
 import Playlist from "./components/playlist/Playlist";
 import Search from "./components/search/Search";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -13,113 +14,18 @@ function App() {
       <div className="container">
         <main className="main">
           <MainNav />
-
           <div className="main__centerblock centerblock">
             <Search />
             <h2 className="centerblock__h2">Треки</h2>
             <Filter className="centerblock__filter" />
             <div className="centerblock__content">
-              <div className="content__title playlist-title">
-                <div className="playlist-title__col col01">Трек</div>
-                <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-                <div className="playlist-title__col col03">АЛЬБОМ</div>
-                <div className="playlist-title__col col04">
-                  <svg className="playlist-title__svg" alt="time">
-                    <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-                  </svg>
-                </div>
-              </div>
+              <PlaylistTitle className="content__title" />
               <Playlist className="content__playlist" />
             </div>
           </div>
           <Sidebar className="main__sidebar" />
         </main>
         <Bar />
-        <div className="bar">
-          <div className="bar__content">
-            <div className="bar__player-progress"></div>
-            <div className="bar__player-block">
-              <div className="bar__player player">
-                <div className="player__controls">
-                  <div className="player__btn-prev">
-                    <svg className="player__btn-prev-svg" alt="prev">
-                      <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-play _btn">
-                    <svg className="player__btn-play-svg" alt="play">
-                      <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-next">
-                    <svg className="player__btn-next-svg" alt="next">
-                      <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-repeat _btn-icon">
-                    <svg className="player__btn-repeat-svg" alt="repeat">
-                      <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-shuffle _btn-icon">
-                    <svg className="player__btn-shuffle-svg" alt="shuffle">
-                      <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-                    </svg>
-                  </div>
-                </div>
-
-                <div className="player__track-play track-play">
-                  <div className="track-play__contain">
-                    <div className="track-play__image">
-                      <svg className="track-play__svg" alt="music">
-                        <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                      </svg>
-                    </div>
-                    <div className="track-play__author">
-                      <a className="track-play__author-link" href="http://">
-                        Ты та...
-                      </a>
-                    </div>
-                    <div className="track-play__album">
-                      <a className="track-play__album-link" href="http://">
-                        Баста
-                      </a>
-                    </div>
-                  </div>
-
-                  <div className="track-play__like-dis">
-                    <div className="track-play__like _btn-icon">
-                      <svg className="track-play__like-svg" alt="like">
-                        <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-                      </svg>
-                    </div>
-                    <div className="track-play__dislike _btn-icon">
-                      <svg className="track-play__dislike-svg" alt="dislike">
-                        <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="bar__volume-block volume">
-                <div className="volume__content">
-                  <div className="volume__image">
-                    <svg className="volume__svg" alt="volume">
-                      <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
-                    </svg>
-                  </div>
-                  <div className="volume__progress _btn">
-                    <input
-                      className="volume__progress-line _btn"
-                      type="range"
-                      name="range"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <Footer />
       </div>
     </div>
