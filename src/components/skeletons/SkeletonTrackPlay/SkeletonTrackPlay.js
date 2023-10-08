d6823d385b345ef9d974ae3f3341932afc1145ce
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-10-08 19:48:44 +0300

    hw-lyfe-cycle-components filter items

---
+++ src/components/skeletons/SkeletonTrackPlay/SkeletonTrackPlay.js
@@ -0,0 +1,11 @@
+import "./SkeletonTrackPlay.css";
+
+export default function SkeletonTrackplay() {
+  return (
+    <div className="skeleton-trackplay__contain">
+      <div className="skeleton-trackplay__image"></div>
+      <div className="skeleton-trackplay__author"></div>
+      <div className="skeleton-trackplay__album"></div>
+    </div>
+  );
+}
