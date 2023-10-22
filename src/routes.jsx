b9e333daed03113c15c3137ec7a7c0d0baa8865f
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-10-22 16:43:34 +0300

    hw-routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,8 +5,18 @@ import { Favorites } from "./pages/Favorites";
 import { Category } from "./pages/Category";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { ProtectedRoute } from "./components/protected-route/ProtectedRoute";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export const AppRoutes = ({ token }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute isAllowed={Boolean(token)} />}>
