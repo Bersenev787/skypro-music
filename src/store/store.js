57172ae04e82c4d739ec5b5ece45a18cc8576e94
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-11-25 13:31:08 +0300

    hw-redux-async add grade

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import trackReducer from "./slices/playList";
 
 export const store = configureStore({
   reducer: {
     track: trackReducer,
   },
+  middleware: [thunk],
 });
